a20858f136ec48b5a1cdf05a1e5780f98814a76a
Saeid Mohadjer <saeid@fastmail.fm>
2015-02-15 21:39:28 +0100

    remove dist and include folders during clean

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function (grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
-		clean: ['./dist/*', './include/*'],
+		clean: ['./dist', './include'],
 
 		markdown: {
 		  all: {
